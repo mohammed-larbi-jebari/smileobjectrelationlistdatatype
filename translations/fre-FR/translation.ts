ac24da366bea9962dddab6de89b5ba6d5e39bad7
Mohammed l'arbi Jebari <arbito82@gmail.com>
2014-08-06 10:10:09 +0100

    extension allow you to use the  object relation list datatype as information collector.

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+  <name>kernel/classes/datatypes</name>
+  
+  <message>
+    <source>smile Object relations</source>
+    <translation>smile Relation d'objets</translation>
+  </message>
+  
+</context>
+</TS>
